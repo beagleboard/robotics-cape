2dee93ef019ec92f86b819b861b0d6d95fccc09c
Jason Kridner <jkridner@beagleboard.org>
2021-11-23 18:48:39 +0000

    build dt before apply

--- scripts/inst-robo-cape.sh
+++ scripts/inst-robo-cape.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/bin/bash -ev
+
+cd /opt/source/BeagleBoard-DeviceTrees
+
+make clean
+
+make
+
+sudo make install
 
 sudo fdtoverlay -i /opt/source/BeagleBoard-DeviceTrees/src/arm64/k3-j721e-beagleboneai64.dtb -o /boot/k3-j721e-beagleboneai64.dtb /opt/source/BeagleBoard-DeviceTrees/src/arm64/overlays/robotics-cape.dtbo
